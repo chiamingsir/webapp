efaae5158efa48e5865b71234e44434c2d0bd5a6
Bob <bob@bogon.dhcp>
2018-08-06 12:58:25 +0800

    fix many bugs

--- www/coroweb.py
+++ www/coroweb.py
@@ -16,7 +16,7 @@ def get(path):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kw):
-            return func(args, kw)
+            return func(*args, **kw)
         wrapper.__method__ = 'GET'
         wrapper.__route__ = path
         return wrapper
@@ -28,7 +28,7 @@ def post(path):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kw):
-            return func(args, kw)
+            return func(*args, **kw)
         wrapper.__method__ = 'POST'
         wrapper.__route__ = path
         return wrapper
@@ -74,6 +74,7 @@ def has_request_arg(fn):
     for name, param in params.items():
         if name == 'request':
             found = True
+            continue
         if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
             raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
@@ -145,7 +146,7 @@ class RequestHandler(object):
 def add_static(app):
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
     app.router.add_static('/static/', path)
-    logging.info('add_static() %s => %s' % ('/static', path))
+    logging.info('add static %s => %s' % ('/static', path))
 
 def add_route(app, fn):
     method = getattr(fn, '__method__', None)
@@ -154,7 +155,7 @@ def add_route(app, fn):
         raise ValueError('@get or @post not defined in %s.' % str(fn))
     if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
         fn = asyncio.coroutine(fn)
-    logging.info('add route %s %s => %s(%s))' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
     app.router.add_route(method, path, RequestHandler(app, fn))
 
 
@@ -165,12 +166,12 @@ def add_routes(app, module_name):
     else:
         name = module_name[n+1:]
         mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
-        for attr in dir(mod):
-            if attr.startswith('_'):
-                continue
-            fn = getattr(mod, attr)
-            if callable(fn):
-                method = getattr(fn, '__method__', None)
-                path = getattr(fn, '__route__', None)
-                if method and path:
-                    add_route(app, fn)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
